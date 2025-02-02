0e51fa8e8337df316e01ee8797fac1479e49ebeb
pphatdev <leatsophat02@gmail.com>
2025-02-02 18:57:37 +0700

    📦: Configured fonts and layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Kantumruy_Pro, Poppins } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const kantumruyPro = Kantumruy_Pro({
+  variable: "--font-kantumruy",
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
+  subsets: ["khmer"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const poppins = Poppins({
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
 });
 
@@ -25,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${kantumruyPro.variable} ${poppins.variable} antialiased`}
       >
         {children}
       </body>
