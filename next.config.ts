cf45b34c360d32c5dffc9823a3507eba14aa3e6f
pphatdev <leatsophat02@gmail.com>
2025-02-02 18:59:39 +0700

    📦: Configured project initialize

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'hebbkx1anhila5yf.public.blob.vercel-storage.com',
+        port: '',
+        pathname: '/*',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
