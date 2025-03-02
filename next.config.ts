f122c510c579c9b38faca6bc79af01c3fbec9033
pphatdev <leatsophat02@gmail.com>
2025-03-02 16:17:50 +0700

    Created: Gallay page

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,27 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['github.com'],
+    domains: ['github.com', 'images.unsplash.com', 'cdn.pixabay.com', 'kxptt4m9j4.ufs.sh'],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'github.com',
         pathname: '/pphatdev.png',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.pixabay.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'kxptt4m9j4.ufs.sh',
+        pathname: '**',
       },
     ],
   },
