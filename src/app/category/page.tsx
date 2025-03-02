bba5aea9cf3e6ad2c40f0753b7a168562b93e2dc
pphatdev <leatsophat02@gmail.com>
2025-03-02 15:44:34 +0700

    💻: Create Category page

---
+++ src/app/category/page.tsx
@@ -0,0 +1,38 @@
+import { Container } from '@/components/container'
+import { Input } from '@/components/ui/input';
+import { SearchIcon } from 'lucide-react';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { contents } from '@/data/books';
+import { BlogCard } from '@/components/ui/blog-card';
+
+export default async function Home() {
+    return (
+        <Container breadcrumb={[{ label: "ទំព័រដើម" }, { label: "រឿងអក្សរសិល្ប៍" }]}>
+            <div className='container mx-auto'>
+                <nav className='flex justify-between items-center'>
+                    <h1 className="text-3xl font-bold">រឿងអក្សរសិល្ប៍</h1>
+                    <div className='flex gap-2'>
+                        <div className="relative">
+                            <Input name='search' placeholder='ស្វែងរក...' className='pl-8'></Input>
+                            <SearchIcon className='size-4 absolute top-2.5 left-2' />
+                        </div>
+                        <Select>
+                            <SelectTrigger className="w-[180px]">
+                                <SelectValue placeholder="ទាំងអស់" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectItem value="all">ទាំងអស់</SelectItem>
+                                <SelectItem value="latest">ថ្មីបំផុត</SelectItem>
+                                <SelectItem value="popular">ពេញនិយម</SelectItem>
+                            </SelectContent>
+                        </Select>
+                    </div>
+                </nav>
+
+                <div className="grid grid-cols-1 mt-10 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-8">
+                    {contents.map((item, key) => <BlogCard item={item} cardKey={key} /> )}
+                </div>
+            </div>
+        </Container>
+    )
+}
